e21184011efe876de65e31d5db481ee73ac96735
heronhoga <nozzeravixer@gmail.com>
2024-12-11 13:54:50 +0700

    fix:fix icons

--- my-components/ukm-sidebar.tsx
+++ my-components/ukm-sidebar.tsx
@@ -4,58 +4,84 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
+//icons
+import { UserPen, Store, LayoutDashboard, Settings } from "lucide-react";
+
 function UkmSidebar({ username }: { username: string }) {
   const handleLogout = () => {
     localStorage.clear();
     window.location.href = "/";
-  }
+  };
   return (
     <div className="w-64 h-screen bg-gradient-to-t from-[#209cff] to-[#68e0cf] flex flex-col text-white">
       {/* Sidebar Header */}
       <div className="flex items-center justify-center p-4 border-b border-gray-700">
         <div id="image-sidebar">
-        <Image
-          src="/images/icon.png"
-          alt="Logo"
-          width={128}
-          height={128}
-        />
+          <Image src="/images/icon.png" alt="Logo" width={128} height={128} />
         </div>
-        <h1 className="text-2xl font-semibold p-3 border rounded-xl border-t-4" style={{textShadow: "2px 2px 2px rgba(0, 0, 0, 1)"}}>UKM</h1>
+        <h1
+          className="text-2xl font-semibold p-3 border rounded-xl border-t-4"
+          style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}
+        >
+          UKM
+        </h1>
       </div>
 
       {/* Sidebar Menu */}
       <div className="flex flex-col p-4 space-y-2">
         <div className="text-lg">
-          <p className="font-semibold text-center text-xl" style={{textShadow: "2px 2px 2px rgba(0, 0, 0, 1)"}}>Welcome, {username}!</p>
+          <p
+            className="font-semibold text-center text-xl"
+            style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}
+          >
+            Halo, {username}!
+          </p>
         </div>
 
         <hr />
 
         <div>
           <Link
-            href="#"
-            className="block py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200"
+            href="/dashboard"
+            className="py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200 flex items-center"
           >
-            Dashboard
+            <LayoutDashboard />
+            <p className="ml-5">Dashboard</p>
           </Link>
+          <Link
+            href="/profile"
+            className="py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200 flex items-center"
+          >
+            <UserPen />
+            <p className="ml-5">Profil</p>
+          </Link>
+
+          <Link
+            href="/ukm"
+            className="py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200 flex items-center"
+          >
+            <Store />
+            <p className="ml-5">Ukm</p>
+          </Link>
+          <Link
+            href="/ukm"
+            className="py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200 flex items-center"
+          >
+            <Settings />
+            <p className="ml-5">Pengaturan</p>
+          </Link>
+          <hr />
           <Link
             href="#"
             className="block py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200"
           >
-            Profile
-          </Link>
-          <Link
-            href="#"
-            className="block py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200"
-          >
-            Settings
-          </Link>
-          <Link
-            href="#"
-            className="block py-2 px-4 rounded-lg hover:bg-gray-700 transition duration-200"
-          >
-            <Button variant={"destructive"} onClick={handleLogout}>Logout</Button>
+            <Button
+              variant={"destructive"}
+              onClick={handleLogout}
+              className="shadow-md"
+            >
+              Logout
+            </Button>
           </Link>
         </div>
       </div>
