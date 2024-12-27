2a9e88194d5c38998f2bfb2b268b18e16f92fdea
heronhoga <nozzeravixer@gmail.com>
2024-12-28 00:06:10 +0700

    fix:fix something

--- app/tentang-kami/page.tsx
+++ app/tentang-kami/page.tsx
@@ -89,7 +89,7 @@ function TentangKami() {
             </h1>
 
             <p className="mt-10 mx-10 text-white font-semibold text-lg sm:text-xl md:text-2xl">
-              Niago merupakan platform yang memberikan kemudahan untuk menampung
+              <span>Niago</span> merupakan platform yang memberikan kemudahan untuk menampung
               investasi dan pengembangan UKM. Kami menyediakan berbagai layanan untuk memudahkan
               pengelolaan investasi bagi investor dan pendanaan UKM!
             </p>
